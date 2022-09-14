8ae41b6114f203182897009460bd658512681cd7
JSSatorres <juansataz.dev@gmail.com>
2022-09-14 12:51:54 +0200

    exercise 1

--- 1.js
+++ 1.js
@@ -2,12 +2,34 @@
 // Dada una matriz de N elementos en la que todos los elementos son iguales excepto uno,
 // crea una función findUniq que retorne el elemento único.
 
-function findUniq(array) {
-}
+const { timesRepeated, isAnArray, isAnArrayEmpty } = require("./utils.js");
 
+function findUniq(array) {
+  isAnArray(array);
+  isAnArrayEmpty(array);
+
+  const objetcTimesRepeated = timesRepeated(array);
+
+  let numberNoRepeat = [];
+  for (const key in objetcTimesRepeated) {
+    if (objetcTimesRepeated[key] === 1) numberNoRepeat.push(key);
+  }
+  return numberNoRepeat;
+}
 
 /**
  * TEST Ejercicio 1
  */
-findUniq(['12', 10, '12', 11, 1, 11, 10, '12']) // 1
-findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']) // 'Wonder Woman'
+findUniq(["12", 10, "12", 11, 1, 11, 10, "12"]); // 1
+
+findUniq([
+  "Capitán América",
+  "Hulk",
+  "Deadpool",
+  "Capitán América",
+  "Hulk",
+  "Wonder Woman",
+  "Deadpool",
+  "Iron Man",
+  "Iron Man",
+]); // 'Wonder Woman'
